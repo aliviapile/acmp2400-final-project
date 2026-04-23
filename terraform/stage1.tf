317a32295ce2ff185e377faa2df22b07ce34691c
aliviapile <apile@unomaha.edu>
2026-04-23 15:17:37 -0500

    Update stage1.tf

--- terraform/stage1.tf
+++ terraform/stage1.tf
@@ -1,17 +1,38 @@
+variable "ARM_SUBSCRIPTION_ID" {}
+variable "ARM_TENANT_ID" {}
+variable "ARM_CLIENT_ID" {}
+variable "ARM_CLIENT_SECRET" {}
+
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.68.0"
+      version = "4.68.0"
     }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-acmp-final"
+    storage_account_name = "acmp2400storageaccount"
+    container_name       = "big-tf-state-acmp2400"
+    key                  = "apile"
+    use_azuread_auth     = true
   }
 }
 
 provider "azurerm" {
   features {}
+
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+  tenant_id       = var.ARM_TENANT_ID
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
 }
 
-resource "azurerm_resource_group" "stage1" {
-  name     = "rg-stage1"
-  location = "East US"
+resource "azurerm_container_registry" "aliviapile_acr" {
+  name                = "acraliviapileacmp2400"
+  resource_group_name = "rg-aliviapile"
+  location            = "Central US"
+  sku                 = "Basic"
+  admin_enabled       = false
 }
