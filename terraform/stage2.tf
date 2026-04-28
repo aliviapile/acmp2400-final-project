f5198ce3b7dcfa1a876dee9155eb0c7a5c5b5974
aliviapile <apile@unomaha.edu>
2026-04-28 11:30:50 -0500

    Remove provider and backend configuration from stage2.tf
    Removed Terraform provider and backend configuration.

--- terraform/stage2.tf
+++ terraform/stage2.tf
@@ -1,25 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.68.0"
-    }
-  }
-
-  backend "azurerm" {
-    resource_group_name  = "rg-acmp-final"
-    storage_account_name = "acmp2400storageaccount"
-    container_name       = "big-tf-state-acmp2400"
-    key                  = "apile"
-    use_azuread_auth     = true
-  }
-}
-
-provider "azurerm" {
-  features {}
-  subscription_id = "f9dd3451-71c1-4bd8-84f8-c83195ce534e"
-}
-
 resource "azurerm_container_group" "app" {
   name                = "acmp2400-container"
   location            = "Central US"
