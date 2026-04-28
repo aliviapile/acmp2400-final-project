f9e5555b4716869edeb3d4cb7255567e9909dc29
aliviapile <apile@unomaha.edu>
2026-04-28 11:26:39 -0500

    Update entrypoint.sh

--- .github/actions/terraform/entrypoint.sh
+++ .github/actions/terraform/entrypoint.sh
@@ -1,10 +1,25 @@
-#!/bin/sh
+#!/bin/bash
+
 set -e
 
-cd terraform
+export ARM_CLIENT_ID=${INPUT_ARM_CLIENT_ID}
+export ARM_CLIENT_SECRET=${INPUT_ARM_CLIENT_SECRET}
+export ARM_SUBSCRIPTION_ID=${INPUT_ARM_SUBSCRIPTION_ID}
+export ARM_TENANT_ID=${INPUT_ARM_TENANT_ID}
+export STATE_KEY=${INPUT_STATE_KEY}
+export TF_STAGE=${INPUT_TF_STAGE}
 
-echo "Running Terraform..."
+if [[ "$TF_STAGE" == "stage1" ]]; then
+  terraform -chdir=terraform init -backend-config="key=${STATE_KEY}.tfstate"
+  terraform -chdir=terraform plan -out=${TF_STAGE}.tfplan
+  terraform -chdir=terraform apply ${TF_STAGE}.tfplan
 
-terraform init
-terraform validate
-terraform plan
+elif [[ "$TF_STAGE" == "stage2" ]]; then
+  terraform -chdir=terraform init -backend-config="key=${STATE_KEY}.tfstate"
+  terraform -chdir=terraform plan \
+    -var="acr_username=${INPUT_ACR_USERNAME}" \
+    -var="acr_password=${INPUT_ACR_PASSWORD}" \
+    -out=${TF_STAGE}.tfplan
+
+  terraform -chdir=terraform apply ${TF_STAGE}.tfplan
+fi
