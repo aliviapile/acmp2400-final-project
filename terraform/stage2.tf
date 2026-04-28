b64f1fc8cc1b1341dcc18b8ac9774e9133028bef
aliviapile <apile@unomaha.edu>
2026-04-28 12:51:07 -0500

    Refactor Azure resources and add variables
    Added variables for ARM client ID, secret, and Django secret key. Updated container group and registry configurations.

--- terraform/stage2.tf
+++ terraform/stage2.tf
@@ -1,34 +1,42 @@
-resource "azurerm_container_group" "app" {
-  name                = "acmp2400-container"
+variable "ARM_CLIENT_ID" {}
+variable "ARM_CLIENT_SECRET" {}
+variable "DJANGO_SECRET_KEY_PROD" {}
+
+resource "azurerm_container_registry" "apile_acr" {
+  name                = "acapileacmp2400"
+  resource_group_name = "rg-apile"
+  location            = "Central US"
+  sku                 = "Basic"
+  admin_enabled       = false
+}
+
+resource "azurerm_container_group" "apile_aci" {
+  name                = "acmp-apile-aci"
   location            = "Central US"
   resource_group_name = "rg-apile"
   ip_address_type     = "Public"
-  dns_name_label      = "apile-acmp2400"
+  dns_name_label      = "acmp-apile-instance"
   os_type             = "Linux"
 
   container {
-    name   = "final-app"
+    name   = "final"
     image  = "acapileacmp2400.azurecr.io/final:latest"
-    cpu    = "1"
+    cpu    = "0.5"
     memory = "1.5"
 
     ports {
       port     = 8000
       protocol = "TCP"
     }
-  }
 
-  exposed_port {
-    port     = 8000
-    protocol = "TCP"
+    secure_environment_variables = {
+      DJANGO_SECRET_KEY = var.DJANGO_SECRET_KEY_PROD
+    }
   }
 
   image_registry_credential {
     server   = "acapileacmp2400.azurecr.io"
-    username = var.acr_username
-    password = var.acr_password
+    username = var.ARM_CLIENT_ID
+    password = var.ARM_CLIENT_SECRET
   }
 }
-
-variable "acr_username" {}
-variable "acr_password" {}
